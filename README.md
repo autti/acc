82ed027b98434046bcbf6c44dc756a53602a10da
Ariel Nuñez <ingenieroariel@gmail.com>
2017-02-02 21:10:22 -0500

    Carried license over from Comma AI

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # acc
-Adaptive Cruise Control
+[![Build Status](https://travis-ci.org/autti/acc.svg?branch=master)](https://travis-ci.org/autti/acc)
+[![Coverage Status](https://coveralls.io/repos/github/autti/acc/badge.svg?branch=master)](https://coveralls.  io/github/autti/acc?branch=master) 
+
+Adaptive Cruise Control. Udacity micro challenge.
+
+# WHAT SHOULD I DO?
+Look for `cruise.py` and implement the `control` function.
+
+# More information
+
+Join the #acc-challenge channel on the ND013 Slack and ask away.
+
+Here are some reference links shared by Mac:
+
+  - https://www.codeproject.com/articles/36459/pid-process-control-a-cruise-control-example
+  - http://itech.fgcu.edu/faculty/zalewski/cda4170/files/pidcontrol.pdf 
+  - https://github.com/slater1/AdaptiveCruiseControl 
+  - https://github.com/commaai/openpilot/blob/master/selfdrive/controls/lib/adaptivecruise.py
+
+# TESTING
+
+```
+python setup.py test
+```
+
+# TODO
+
+ - [ ] Wire cruise.control in runtracks so that the control function is passed to the Plant on initialization
+ - [ ] Remove dependency on live20, capnpn, can messages and other openpilot specific bits
+ - [ ] Enable running the maneuvers on TravisCI
+ - [ ] Create assertions for reasonable behavior when implementing the maneuver and fail the tests when those do not pass.
---
+++ acc/tests/maneuver.py
@@ -0,0 +1,69 @@
+import os
+from enum import Enum
+from plant import Plant
+import numpy as np
+
+
+class Maneuver(object):
+
+    def __init__(self, title, duration, **kwargs):
+        # Was tempted to make a builder class
+        self.distance_lead = kwargs.get("initial_distance_lead", 200.0)
+        self.speed = kwargs.get("initial_speed", 0.0)
+        self.lead_relevancy = kwargs.get("lead_relevancy", 0)
+
+        self.grade_values = kwargs.get("grade_values", [0.0, 0.0])
+        self.grade_breakpoints = kwargs.get(
+            "grade_breakpoints", [0.0, duration])
+        self.speed_lead_values = kwargs.get("speed_lead_values", [0.0, 0.0])
+        self.speed_lead_breakpoints = kwargs.get(
+            "speed_lead_values", [0.0, duration])
+
+        self.cruise_button_presses = kwargs.get("cruise_button_presses", [])
+
+        self.duration = duration
+        self.title = title
+
+    def evaluate(self, control=None, verbosity=0):
+        """runs the plant sim and returns (score, run_data)"""
+        plant = Plant(
+            lead_relevancy=self.lead_relevancy,
+            speed=self.speed,
+            distance_lead=self.distance_lead,
+            verbosity=verbosity,
+        )
+
+        event_queue = sorted(self.cruise_button_presses,
+                             key=lambda a: a[1])[::-1]
+
+        buttons_sorted = sorted(self.cruise_button_presses, key=lambda a: a[1])
+        current_button = 0
+
+        brake = 0
+        gas = 0
+        steer_torque = 0
+
+        while plant.current_time() < self.duration:
+            while buttons_sorted and plant.current_time() >= buttons_sorted[0][1]:
+                current_button = buttons_sorted[0][0]
+                buttons_sorted = buttons_sorted[1:]
+                if verbosity > 1:
+                    print("current button changed to", current_button)
+
+            grade = np.interp(plant.current_time(),
+                              self.grade_breakpoints, self.grade_values)
+            speed_lead = np.interp(
+                plant.current_time(), self.speed_lead_breakpoints, self.speed_lead_values)
+
+            speed, acceleration, car_in_front, steer_torque = plant.step(brake=brake,
+                                                                         gas=gas,
+                                                                         v_lead=speed_lead,
+                                                                         cruise_buttons=current_button,
+                                                                         grade=grade)
+
+            brake, gas = control(speed, acceleration,
+                                 car_in_front, steer_torque)
+
+            v_rel = speed_lead - speed if self.lead_relevancy else 0.
+
+        return None
